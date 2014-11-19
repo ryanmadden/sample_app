3cfe7099c389cda62830a5358eca8ba7a6dc8b11
Ryan Madden <ryanmadden2017@u.northwestern.edu>
2014-11-19 00:06:07 -0600

    Add about page and associated test

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   get 'static_pages/help'
 
+  get 'static_pages/about'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -15,7 +16,7 @@ Rails.application.routes.draw do
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
-  # Example resource route (maps HTTP verbs to controller actions automatically):
+  # Example resource route (maps HTTP verbs to controllers actions automatically):
   #   resources :products
 
   # Example resource route with options:
